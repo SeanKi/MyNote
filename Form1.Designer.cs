ee99d35b2f6f03e4b667e2d3488c5fde38dad83e
SeanKi <ykish72@gmail.com>
2022-03-20 14:55:18 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace NSXNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.htmlEditControl1 = new Zoople.HTMLEditControl();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(4, 92);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(369, 842);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1103, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 33);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1103, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripContainer1
+            // 
+            this.toolStripContainer1.BottomToolStripPanelVisible = false;
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(8, 0);
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(13, 69);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(8, 8);
+            this.toolStripContainer1.TabIndex = 4;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(191, 29);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // htmlEditControl1
+            // 
+            this.htmlEditControl1.AcceptsReturn = true;
+            this.htmlEditControl1.AllowDragInternal = true;
+            this.htmlEditControl1.AvailableInlineStyles = ((System.Collections.Generic.List<string>)(resources.GetObject("htmlEditControl1.AvailableInlineStyles")));
+            this.htmlEditControl1.BaseURL = null;
+            this.htmlEditControl1.CleanMSWordHTMLOnPaste = true;
+            this.htmlEditControl1.CodeEditor.Enabled = true;
+            this.htmlEditControl1.CodeEditor.Locked = false;
+            this.htmlEditControl1.CodeEditor.TabWidth = 720;
+            this.htmlEditControl1.CodeEditor.WordWrap = false;
+            this.htmlEditControl1.CSSText = null;
+            this.htmlEditControl1.CustomColorPalette = null;
+            this.htmlEditControl1.DocumentHTML = null;
+            this.htmlEditControl1.EditingDisabled = false;
+            this.htmlEditControl1.EnableInlineSpelling = false;
+            this.htmlEditControl1.FontSizesList = null;
+            this.htmlEditControl1.FontsList = null;
+            this.htmlEditControl1.HiddenButtons = null;
+            this.htmlEditControl1.ImageStorageLocation = null;
+            this.htmlEditControl1.InCodeView = false;
+            this.htmlEditControl1.LanguageFile = null;
+            this.htmlEditControl1.LicenceActivationKey = null;
+            this.htmlEditControl1.LicenceKey = null;
+            this.htmlEditControl1.LicenceKeyInlineSpelling = null;
+            this.htmlEditControl1.Location = new System.Drawing.Point(379, 534);
+            this.htmlEditControl1.Name = "htmlEditControl1";
+            this.htmlEditControl1.Size = new System.Drawing.Size(724, 400);
+            this.htmlEditControl1.SpellingAutoCorrectionList = ((System.Collections.Hashtable)(resources.GetObject("htmlEditControl1.SpellingAutoCorrectionList")));
+            this.htmlEditControl1.TabIndex = 5;
+            this.htmlEditControl1.ToolstripImageScalingSize = new System.Drawing.Size(24, 24);
+            this.htmlEditControl1.UseParagraphAsDefault = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(379, 103);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(724, 394);
+            this.webBrowser1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 934);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.htmlEditControl1);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private Zoople.HTMLEditControl htmlEditControl1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NSXNote
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //if (webBrowser1.Document != null) {
+                //    object html = webBrowser1.Document.InvokeScript
+
+                webBrowser1.Navigate("");
+                Application.DoEvents();
+                webBrowser1.Document.OpenNew(false).Write("<html><body><div><span>Date</span>2022-03-20</div><div id=\"editable\">Edit this text</div></body></html>");
+
+                foreach (HtmlElement el in webBrowser1.Document.All)
+                {
+                    el.SetAttribute("unselectable", "on");
+                    el.SetAttribute("contenteditable", "false");
+                }
+
+                webBrowser1.Document.Body.SetAttribute("width", this.Width.ToString() + "px");
+                webBrowser1.Document.Body.SetAttribute("height", "100%");
+                webBrowser1.Document.Body.SetAttribute("contenteditable", "true");
+                webBrowser1.Document.DomDocument.GetType().GetProperty("designMode").SetValue(webBrowser1.Document.DomDocument, "On", null);
+                webBrowser1.IsWebBrowserContextMenuEnabled = false;
+
+                JObject o1 = JObject.Parse(File.ReadAllText("config.json"));
+                var a = o1;
+                foreach (JProperty property in o1.Properties())
+                {
+                    string name = property.Name;
+                    var n = treeView1.Nodes.Add(name);
+                    foreach (var item in property.Value)
+                    {
+                        Console.WriteLine($"Value: {item}");
+                        n.Nodes.Add(item.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text != "")
+            {
+                try
+                {
+                    JObject o1 = JObject.Parse(File.ReadAllText(e.Node.Text));
+                    // title
+                    // brief
+                    // content
+                    if (o1["category"] != null)
+                    {
+                        var category = o1["category"].ToString();
+                        var title = o1["title"].ToString();
+                        Console.WriteLine($"{category} {title}");
+                    }
+                    if (o1["content"] != null)
+                    {
+                        var content = o1["content"].ToString();
+                        var brief = o1["brief"].ToString();
+                        webBrowser1.Document.GetElementById("editable").InnerHtml = content;
+                    }
+                    //htmlEditControl1.DocumentHTML = content;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
